9e61f2fc2e299952e465107b3336da2d1196cf51
Nkwuda Theophilus <theophilusonyebuchinkwuda@gmail.com>
2025-01-24 11:06:37 +0100

    implement project file structure

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default async function Home() {
+ 
+
+  return (
+    <h1 className="text-green-700">Hello email client</h1>
+  );
+}
---
+++ src/playground.ts
@@ -0,0 +1,11 @@
+// import { db } from "./server/db";
+
+// await db.User.create({
+//     data:{
+//         emailAddress: 'test@gmail.com',
+//         firstName: 'test',
+//         lastName: 'test',
+//     }
+// });
+
+// console.log('done');
